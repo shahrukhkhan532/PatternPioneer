2997bc41b0c06e581db1032aba1a2cceb34e8d7b
shahrukh15 <87074409+shahrukh15@users.noreply.github.com>
2023-10-10 19:56:38 +0500

    Create IUserService.cs

---
+++ Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿namespace PatternPioneer.Services
+{
+    public interface IUserService
+    {
+        (string FirstName, string LastName) GetById(int Id);
+    }
+
+    public class UserService : IUserService
+    {
+        public (string FirstName, string LastName) GetById(int Id)
+        {
+            return ("John", "Doe");
+        }
+    }
+}
