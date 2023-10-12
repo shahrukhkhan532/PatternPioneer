f1a2ee44c835bdcff1ad8f89441efe1d4bc77f90
shahrukh15 <87074409+shahrukh15@users.noreply.github.com>
2023-10-12 20:56:27 +0500

    shah_rukh_12-10-2023

---
+++ Scripts/mysampleScript.ps1
@@ -0,0 +1,6 @@
+﻿        $changedFiles = git diff --name-only HEAD~2 HEAD
+
+        foreach ($file in $changedFiles) {
+            echo $file.Name
+             Move-Item -Path $file -Destination ".\DB Script\"
+        }
