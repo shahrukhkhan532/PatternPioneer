7c7c2b2f050f5000229ea8f0cfdbcddf6aaa40f0
shahrukh15 <87074409+shahrukh15@users.noreply.github.com>
2023-10-16 19:08:45 +0500

    Delete mysampleScript.ps1

--- Scripts/mysampleScript.ps1
+++
@@ -1,6 +0,0 @@
-﻿        $changedFiles = git diff --name-only HEAD~2 HEAD
-
-        foreach ($file in $changedFiles) {
-            echo $file.Name
-             Move-Item -Path $file -Destination ".\DB Script\"
-        }
