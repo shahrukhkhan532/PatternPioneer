6051c395923e7372690ad8f72af87501db94747e
Shah Rukh Khan <khanxhahrukh@gmail.com>
2024-06-09 10:50:20 -0700

    Create DecoratorPatternController.cs

---
+++ Controllers/DecoratorPatternController.cs
@@ -0,0 +1,33 @@
+﻿using PatternPioneer.DecoratorPattern;
+
+namespace PatternPioneer.Controllers;
+
+[Route("[controller]/[action]")]
+[ApiController]
+public class DecoratorPatternController : ControllerBase
+{
+    private readonly IRepository _repository;
+
+    public DecoratorPatternController(IRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get([FromQuery] int id)
+    {
+        var student = await _repository.GetPersonByIdAsync(id);
+        return Ok(student);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Post([FromQuery] int id, string name)
+    {
+        await _repository.SavePersonAsync(new Student()
+        {
+            Id = id,
+            Name = name
+        });
+        return Ok("Success");
+    }
+}
