7de03a1c5de1acea7394ced014002ffd53eabcfb
Shah Rukh Khan <khanxhahrukh@gmail.com>
2024-06-09 10:10:52 -0700

    Update DefaultMessageBuilder.cs

--- Strategies/MessageBuilders/DefaultMessageBuilder.cs
+++ Strategies/MessageBuilders/DefaultMessageBuilder.cs
@@ -1,12 +1,11 @@
-﻿namespace PatternPioneer.Strategies.MessageBuilders
-{
-    public class DefaultMessageBuilder : MessageBuilderBase
-    {
-        public override bool CanHandle(Domain.Entities.Rule rule) => true;
+﻿namespace PatternPioneer.Strategies.MessageBuilders;
 
-        protected override string AddDetails()
-        {
-            return "We were unable to find a suitable strategy for you.";
-        }
+public class DefaultMessageBuilder : MessageBuilderBase
+{
+    public override bool CanHandle(Domain.Entities.Rule rule) => true;
+
+    protected override string AddDetails()
+    {
+        return "We were unable to find a suitable strategy for you.";
     }
 }
